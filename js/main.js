ad05db15fe7c1f483339970a5dbfd73bb1c6ca63
DeliriousIdea <63449281+DeliriousIdea@users.noreply.github.com>
2024-07-30 09:56:39 -0600

    Fixed odd margining

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 const menuButtonRef = document.querySelector(".menu-button")
+const menuListRef = document.querySelectorAll(".menu-list")
 
-console.log(menuButtonRef)
+menuButtonRef.addEventListener('click', toggleNavMenu)
+
+function toggleNavMenu(e) {
+    e.preventDefault()
+    menuListRef.classList.toggle('menu-visible')
+    menuListRef.classList.toggle('menu-button')
+}
